3e3aac6410b1e5c838c7e5807719dfbd1e5d3803
jiapinghui <772344928@qq.com>
2015-11-24 15:59:58 +0800

    no message

--- swiftDemo2/swiftDemo2/AppDelegate.swift
+++ swiftDemo2/swiftDemo2/AppDelegate.swift
@@ -19,10 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         // Override point for customization after application launch.
         
-        // 如果没有授权请求用户授权
-        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.NotDetermined {
-            CLLocationManager().requestWhenInUseAuthorization()
-        }
+        
         
         let first = RJFirstViewController()
         let firstNav: UINavigationController = UINavigationController(rootViewController: first)
--- swiftDemo2/swiftDemo2/Controllers/Four/RJFourViewController.swift
+++ swiftDemo2/swiftDemo2/Controllers/Four/RJFourViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
-class RJFourViewController: RJBaseViewController, CLLocationManagerDelegate {
+class RJFourViewController: RJBaseViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
-    var locationManager: CLLocationManager = CLLocationManager()
+    var locationManager: CLLocationManager?
+    var mpView: MKMapView?
+    var geocoder: CLGeocoder?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,49 +22,69 @@ class RJFourViewController: RJBaseViewController, CLLocationManagerDelegate {
         self.title = "我的财富"
         // Do any additional setup after loading the view.
         
+        self.HUDDefaultShow("正在定位您的当前位置", hiden: false)
+        
+        self.locationManager = CLLocationManager()
+        self.geocoder = CLGeocoder()
+        
         if !CLLocationManager.locationServicesEnabled() {
+            self.HUD?.hidden = true
             self.HUDTextShow("定位服务尚未开启，请设置打开", hiden: true)
             return
         }
-        
-         if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse {
-            self.locationManager.delegate = self
-            // 设置定位精度
-            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            //定位平率
-            let distance: CLLocationDistance = 10.0
-            self.locationManager.distanceFilter = distance
-            // 启动定位
-            self.locationManager.startUpdatingLocation()
+        // 如果没有授权请求用户授权
+        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.NotDetermined {
+            self.locationManager!.requestWhenInUseAuthorization()
+            self.locationManagerUpdatingLocation()
+        } else if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse {
+            self.locationManagerUpdatingLocation()
         }
         
+    }
+    
+    func locationManagerUpdatingLocation() {
+        self.locationManager!.delegate = self
+        // 设置定位精度
+        self.locationManager!.desiredAccuracy = kCLLocationAccuracyBest
+        //定位平率
+        let distance: CLLocationDistance = 10.0
+        self.locationManager!.distanceFilter = distance
+        // 启动定位
+        self.locationManager!.startUpdatingLocation()
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location:CLLocation = locations[0] as CLLocation// 取出第一个位置
-        
-        let coordinate: CLLocationCoordinate2D = location.coordinate
-        
-        self.HUDTextShow(String(format: "经度：%f, 纬度：%f, 海拔：%f, 航向：%f, 速度：%f", coordinate.longitude, coordinate.latitude, location.altitude, location.course, location.speed), hiden: true)
-        
-        self.locationManager.stopUpdatingLocation()
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.getAddressByLocation(location)
+//        let coordinate: CLLocationCoordinate2D = location.coordinate
+//        self.HUDTextShow(String(format: "经度：%f, 纬度：%f, 海拔：%f, 航向：%f, 速度：%f", coordinate.longitude, coordinate.latitude, location.altitude, location.course, location.speed), hiden: false)
+        self.locationManager!.stopUpdatingLocation()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getAddressByLocation(location: CLLocation) {
+        var p:CLPlacemark?
+        self.geocoder!.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+            self.HUD?.hidden = true
+            if error != nil {
+                self.HUDTextShow("reverse geodcode fail", hiden: true)
+                print("reverse geodcode fail: \(error!.localizedDescription)")
+                return
+            }
+            let pm = placemarks! as [CLPlacemark]
+            if (pm.count > 0){
+                p = placemarks![0] as CLPlacemark
+                let dict: NSDictionary = ((p?.addressDictionary)! as NSDictionary)
+                let str: String = String(dict.objectForKey("Country")!) + String(dict.objectForKey("State")!) + String(dict.objectForKey("SubLocality")!) + String(dict.objectForKey("Street")!)
+                self.HUDTextShow(str, hiden: false)
+            }else{
+                self.HUDTextShow("定位失败", hiden: true)
+            }
+        })
     }
-    */
+    
+    func geocodeAddressString(address: String) {
+        
+    }
+    
 
 }
